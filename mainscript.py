e5b984d78dda4076aa68b6c30aa9d6ffca3fdeb9
Pvljk <oorzakpavel10@gmail.com>
2024-03-10 21:57:02 +0700

    добавил функционал кнопки Просмотреть профиль

--- mainscript.py
+++ mainscript.py
@@ -1,3 +1,14 @@
+import os
+import webbrowser
 import eel
+
 eel.init('web')
+
+@eel.expose
+def view_profile():
+    #os.getdwd - позволяет получить путь к текущей папке
+    #+'/knownfaces - добаляет к текущей пути папку knownfaces
+    path = (os.getcwd()+'/knownfaces')
+    #webbrowser.open(path) - открывает папку knownfaces с профилями
+    webbrowser.open(path)
 eel.start('main.html')
--- otherscript.py
+++
@@ -1,7 +0,0 @@
-import os
-import webbrowser
-#os.getdwd - позволяет получить путь к текущей папке
-#+'/knownfaces - добаляет к текущей пути папку knownfaces
-path = (os.getcwd()+'/knownfaces')
-#webbrowser.open(path) - открывает папку knownfaces с профилями
-webbrowser.open(path)
