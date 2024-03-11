6cfdd231101120265577482394d9a20706eb3078
Pvljk <oorzakpavel10@gmail.com>
2024-03-11 10:57:33 +0700

    Добавил функционал кнопке Просмотреть посещаемость

--- mainscript.py
+++ mainscript.py
@@ -10,4 +10,12 @@ def view_profile():
     path = (os.getcwd()+'/knownfaces')
     #webbrowser.open(path) - открывает папку knownfaces с профилями
     os.system("start "+path)
+
+@eel.expose
+def view_atten():
+    #os.getdwd - позволяет получить путь к текущей папке
+    #+'/knownfaces - добаляет к текущей пути папку knownfaces
+    file_path = (os.getcwd()+'/Attendance.csv')
+    #webbrowser.open(path) - открывает папку knownfaces с профилями
+    os.system("start "+file_path)
 eel.start('main.html')
