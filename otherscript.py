4febf1adc4391b1b7e536efe354a57afaf5e40e2
Pvljk <oorzakpavel10@gmail.com>
2024-03-20 08:19:37 +0700

    final stde

--- otherscript.py
+++ otherscript.py
@@ -1,12 +1,89 @@
 import os
 import eel
+import numpy as np
+import face_recognition
+import cv2
+from datetime import datetime
+import re
+import sys
 
 eel.init('web')
 
 @eel.expose
+def camera():
+    path = 'knownfaces'
+    images = []
+    classNames = []
+    myList = os.listdir(path)
+    print(myList)
+
+    for cls in myList:
+        curImg = cv2.imread(f'{path}/{cls}')
+        images.append(curImg)
+        classNames.append(os.path.splitext(cls)[0])
+
+    print(classNames)
+
+    def findEncodings(images):
+        encodeList = []
+        for img in images:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            encode = face_recognition.face_encodings(img)[0]
+            encodeList.append(encode)
+        return encodeList
+
+    def markAttendance(name):
+        with open("Attendance.csv", "r+") as f:
+            myDataList = f.readlines()
+            nameList = []
+            for line in myDataList:
+                entry = line.split(',')
+                nameList.append(entry[0])
+            if name not in nameList:
+                now = datetime.now()
+                dtString = now.strftime("%H:%M:%S")
+                f.writelines(f'\n{name}, {dtString}')
+
+    encodeListKnown = findEncodings(images)
+    print("Декодирование закончено")
+
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        success, img = cap.read()
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+        for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            #print(faceDis)
+            matchIndex = np.argmin(faceDis)
+
+            if matches[matchIndex]:
+                name = classNames[matchIndex]
+                #print(name)
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                markAttendance(name)
+
+        cv2.imshow("WebCam", img)
+        cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+@eel.expose
 def view_profile():
     #os.getdwd - позволяет получить путь к текущей папке
-    #+'/knownfaces - добаляет к текущей пути папку knownfaces
+    #+'/knownfaces - добавляем к текущей пути папку knownfaces
     path = (os.getcwd()+'/knownfaces')
     #webbrowser.open(path) - открывает папку knownfaces с профилями
     os.system("start "+path)
@@ -14,8 +91,35 @@ def view_profile():
 @eel.expose
 def view_atten():
     #os.getdwd - позволяет получить путь к текущей папке
-    #+'/knownfaces - добаляет к текущей пути папку knownfaces
+    #+'/knownfaces - добаляем к текущей пути папку knownfaces
     file_path = (os.getcwd()+'/Attendance.csv')
     #webbrowser.open(path) - открывает папку knownfaces с профилями
     os.system("start "+file_path)
+    
+@eel.expose                         # Expose this function to Javascript
+def handleinput(name):
+    # Откройте веб-камеру
+    cap = cv2.VideoCapture(0)
+
+    # Запросите имя пользователя
+
+    # Создайте путь к папке для сохранения изображений
+
+    save_path = (os.getcwd()+'/knownfaces')
+    # Снимите кадр с веб-камеры
+    ret, frame = cap.read()
+    count = 1
+    # Если кадр не пустой, сделайте снимок
+    if ret:
+        if os.path.exists(os.path.join(save_path, f'{name}.jpg')):
+        # Если файл существует, добавьте номер к имени
+            name = re.sub('\d+', '', name) + f'{count}'
+            count += 1
+    
+        # Сохраните снимок в папку
+        cv2.imwrite(os.path.join(save_path, f'{name}.jpg'), frame)
+
+    # Завершите программу
+    cap.release()
+    cv2.destroyAllWindows()
 eel.start('main1.html')
