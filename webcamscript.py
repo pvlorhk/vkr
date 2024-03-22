5b190bf238518b3ceb2fb111b184e958ad52eed9
Pvljk <oorzakpavel10@gmail.com>
2024-03-22 18:05:49 +0700

    Изменил размер шрифта

--- webcamscript.py
+++ webcamscript.py
@@ -1,8 +1,11 @@
+import os
+import eel
 import numpy as np
 import face_recognition
 import cv2
-import os
 from datetime import datetime
+import re
+
 
 path = 'knownfaces'
 images = []
@@ -14,7 +17,6 @@ for cls in myList:
     curImg = cv2.imread(f'{path}/{cls}')
     images.append(curImg)
     classNames.append(os.path.splitext(cls)[0])
-
 print(classNames)
 
 def findEncodings(images):
@@ -41,30 +43,30 @@ encodeListKnown = findEncodings(images)
 print("Декодирование закончено")
 
 cap = cv2.VideoCapture(0)
-
 while True:
-    success, img = cap.read()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+        success, img = cap.read()
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        #print(faceDis)
-        matchIndex = np.argmin(faceDis)
+        for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]:
-            name = classNames[matchIndex]
-            #print(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            markAttendance(name)
-
-    cv2.imshow("WebCam", img)
-    cv2.waitKey(1)
+            if matches[matchIndex]:
+                name = classNames[matchIndex]
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                markAttendance(name)
+        cv2.imshow("WebCam", img)
+        cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+cap.release()
+cv2.destroyAllWindows()
